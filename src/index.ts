960ea89521727f6ae7bc06312058365114808cee
Kei Ito <7359068+gjbkz@users.noreply.github.com>
2024-09-08 16:07:23 +0900

    deps: upgrade dependencies (#691)
    * deps: typescript:4.9.5→5.5.4
    
    * deps: uninstall node-fetch
    
    * deps: @types/node:18.15.0→22.5.4
    
    * deps: uninstall chokidar
    
    * deps: @nlib/changelog:0.2.4→0.3.1 @nlib/eslint-config:3.19.6→3.20.5 @nlib/githooks:0.1.0→0.2.0 @nlib/lint-commit:0.2.0→1.0.1
    
    * deps: @types/connect:3.4.35→3.4.38
    
    * deps: middleware-static-livereload:1.2.21→1.3.0
    
    * deps: uninstall eslint
    
    * chore: use biome
    
    * deps: commander:9.5.0→12.1.0
    
    * build: use cjs
    
    * test: skip the startServer test
    
    * ci: update node versions
    
    * deps: uninstall githooks
    
    * ci: set concurrency
    
    * test: close manually
    
    * test: timeout 10s
    
    * ci: use v4 actions
    
    * test: use AbortController
    
    * chore: remove console.info
    
    * ci: set timeout-minutes
    
    * ci: build before test
    
    * test: use the kill commands
    
    * test: use kill as default
    
    * test: print the result
    
    * test: print test hooks
    
    * test:try SIGTERM
    
    * test: try detached
    
    * ci: omit ubuntu-latest

--- src/index.ts
+++ src/index.ts
@@ -1,62 +1,62 @@
-import * as http from 'http';
-import {createRequire} from 'module';
-import type * as Connect from 'connect';
-import * as staticLivereload from 'middleware-static-livereload';
+import * as http from "http";
+import type * as Connect from "connect";
+import * as staticLivereload from "middleware-static-livereload";
 
-const require = createRequire(import.meta.url);
+export { LogLevel } from "middleware-static-livereload";
 
-export {LogLevel} from 'middleware-static-livereload';
-
-export interface SableOptions extends staticLivereload.Options {
-    /**
-     * The first argument of server.listen()
-     * https://nodejs.org/api/net.html#net_server_listen_port_host_backlog_callback
-     * @default 4000
-     */
-    port?: number,
-    /**
-     * The second argument of server.listen()
-     * https://nodejs.org/api/net.html#net_server_listen_port_host_backlog_callback
-     * @default undefined
-     */
-    host?: string,
-    /**
-     * A list of middlewares.
-     * @default []
-     */
-    middlewares?: Array<Connect.HandleFunction>,
+export interface SableOptions
+	extends Partial<staticLivereload.MiddlewareOptions> {
+	/**
+	 * The first argument of server.listen()
+	 * https://nodejs.org/api/net.html#net_server_listen_port_host_backlog_callback
+	 * @default 4000
+	 */
+	port?: number;
+	/**
+	 * The second argument of server.listen()
+	 * https://nodejs.org/api/net.html#net_server_listen_port_host_backlog_callback
+	 * @default undefined
+	 */
+	host?: string;
+	/**
+	 * A list of middlewares.
+	 * @default []
+	 */
+	middlewares?: Array<Connect.HandleFunction>;
 }
 
 export const startServer = async (
-    options: SableOptions = {},
+	options: SableOptions = {},
 ): Promise<http.Server> => {
-    const app = (require('connect') as () => Connect.Server)();
-    for (const middleware of options.middlewares || []) {
-        app.use(middleware);
-    }
-    app.use(staticLivereload.middleware(options));
-    const server = http.createServer(app);
-    return await new Promise<http.Server>((resolve, reject) => {
-        server.once('listening', () => {
-            const addressInfo = server.address();
-            if (addressInfo && typeof addressInfo === 'object') {
-                const {address, family, port} = addressInfo;
-                const portSuffix = port === 80 ? '' : `:${port}`;
-                const hostname = options.host || (portSuffix && family === 'IPv6' ? `[${address}]` : address);
-                process.stdout.write(`http://${hostname}${portSuffix}\n`);
-            }
-            resolve(server);
-        });
-        const listen = (port: number, host?: string) => {
-            server.once('error', (error: Error & {code: string}) => {
-                if (error.code === 'EADDRINUSE') {
-                    listen(port + 1, host);
-                } else {
-                    reject(error);
-                }
-            });
-            server.listen(port, host);
-        };
-        listen(options.port || 4000, options.host);
-    });
+	const app = (require("connect") as () => Connect.Server)();
+	for (const middleware of options.middlewares || []) {
+		app.use(middleware);
+	}
+	app.use(staticLivereload.middleware(options));
+	const server = http.createServer(app);
+	return await new Promise<http.Server>((resolve, reject) => {
+		server.once("listening", () => {
+			const addressInfo = server.address();
+			if (addressInfo && typeof addressInfo === "object") {
+				const { address, family, port } = addressInfo;
+				const portSuffix = port === 80 ? "" : `:${port}`;
+				const hostname =
+					options.host ||
+					(portSuffix && family === "IPv6" ? `[${address}]` : address);
+				process.stdout.write(`http://${hostname}${portSuffix}\n`);
+			}
+			resolve(server);
+		});
+		const listen = (port: number, host?: string) => {
+			server.once("error", (error: Error & { code: string }) => {
+				if (error.code === "EADDRINUSE") {
+					listen(port + 1, host);
+				} else {
+					reject(error);
+				}
+			});
+			server.listen(port, host);
+		};
+		listen(options.port || 4000, options.host);
+	});
 };
--- test/index.mjs
+++ test/index.mjs
@@ -1,110 +1,146 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import * as childProcess from 'child_process';
-import * as stream from 'stream';
-import {fileURLToPath} from 'url';
-import test from 'ava';
-import fetch from 'node-fetch';
-// eslint-disable-next-line import/no-relative-parent-imports
-import {startServer} from '../lib/index.mjs';
+//@ts-check
+import * as childProcess from "node:child_process";
+import * as stream from "node:stream";
+import { test } from "node:test";
+import * as assert from "node:assert/strict";
 
-const cwd = new URL('.', import.meta.url);
+const cwd = new URL(".", import.meta.url);
+/** @type {Set<{name: string, fn: () => void | Promise<void>}>} */
+const closeFunctions = new Set();
+const onClose = async () => {
+	const total = closeFunctions.size;
+	for (const { name, fn } of closeFunctions) {
+		const index = total - closeFunctions.size;
+		console.info(`closing ${name} (${index + 1}/${total})`);
+		await fn();
+		console.info(`closed ${name} (${index + 1}/${total})`);
+	}
+	closeFunctions.clear();
+};
+
+/**
+ * @param {string} command
+ * @returns {Promise<{localUrl: URL, abc: AbortController}>}
+ */
+const start = async (command) => {
+	const abc = new AbortController();
+	closeFunctions.add({ name: "AbortController", fn: () => abc.abort() });
+	const timeoutMs = 10000;
+	const timerId = setTimeout(() => abc.abort(), timeoutMs);
+	abc.signal.addEventListener("abort", () => clearTimeout(timerId));
+	const child = childProcess.spawn(`npx ${command}`, {
+		cwd,
+		shell: true,
+		detached: process.platform !== "win32",
+	});
+	const kill = () => {
+		console.info(`stopping ${child.pid} (${process.platform})`);
+		let command = "";
+		switch (process.platform) {
+			case "win32":
+				command = `taskkill /pid ${child.pid} /f /t`;
+				break;
+			default:
+				command = `kill -15 ${child.pid}`;
+				break;
+		}
+		if (command) {
+			console.info(`executing: ${command}`);
+			childProcess.spawnSync(command, { stdio: "inherit", shell: true });
+		}
+		console.info(`stopped ${child.pid} (${process.platform})`);
+	};
+	abc.signal.addEventListener("abort", kill);
+	const localUrl = await new Promise((resolve, reject) => {
+		const chunks = [];
+		let totalLength = 0;
+		const watcher = new stream.Writable({
+			write(chunk, _encoding, callback) {
+				console.info(`${chunk}`);
+				check(chunk);
+				callback();
+			},
+			final(callback) {
+				reject(
+					new Error(
+						`Failed to get a local URL: ${Buffer.concat(chunks, totalLength)}`,
+					),
+				);
+				callback();
+			},
+		});
+		/** @param {Buffer} chunk */
+		const check = (chunk) => {
+			chunks.push(chunk);
+			totalLength += chunk.length;
+			const concatenated = Buffer.concat(chunks, totalLength);
+			const matched = /http:\/\/\S+/.exec(`${concatenated}`);
+			if (matched) {
+				child.stdout.unpipe(watcher);
+				child.stderr.unpipe(watcher);
+				resolve(new URL(matched[0]));
+			}
+		};
+		child.stdout.pipe(watcher);
+		child.stderr.pipe(watcher);
+	});
+	return { localUrl, abc };
+};
 
 test.before(() => {
-    childProcess.execSync('npm install --no-save', {cwd, stdio: 'inherit'});
+	console.info("---- test.before ----");
+	childProcess.execSync("npm install --no-save", { cwd, stdio: "inherit" });
 });
 
-test.beforeEach((beforeT) => {
-    beforeT.context.start = async (t, command) => {
-        const process = t.context.process = childProcess.spawn(
-            `npx ${command}`,
-            {cwd, shell: true},
-        );
-        const localURL = await new Promise((resolve, reject) => {
-            const chunks = [];
-            let totalLength = 0;
-            const check = (chunk) => {
-                chunks.push(chunk);
-                totalLength += chunk.length;
-                const concatenated = Buffer.concat(chunks, totalLength);
-                const matched = (/http:\/\/\S+/).exec(`${concatenated}`);
-                if (matched) {
-                    resolve(new URL(matched[0]));
-                }
-            };
-            process.stdout.pipe(new stream.Writable({
-                write(chunk, _encoding, callback) {
-                    check(chunk);
-                    callback();
-                },
-                final(callback) {
-                    reject(new Error(`Failed to get a local URL: ${Buffer.concat(chunks, totalLength)}`));
-                    callback();
-                },
-            }));
-            process.stderr.pipe(new stream.Writable({
-                write(chunk, _encoding, callback) {
-                    check(chunk);
-                    callback();
-                },
-            }));
-        });
-        return localURL;
-    };
+test.beforeEach(async () => {
+	console.info("---- test.beforeEach ----");
 });
 
-test.afterEach(async (t) => {
-    if (t.context.process) {
-        t.context.process.kill();
-    }
-    const {server} = t.context;
-    if (server) {
-        await new Promise((resolve, reject) => {
-            server.close((error) => {
-                if (error) {
-                    reject(error);
-                } else {
-                    resolve();
-                }
-            });
-        });
-    }
+test.afterEach(async () => {
+	console.info("---- test.afterEach ----");
+	await onClose();
+});
+
+test.after(async () => {
+	console.info("---- test.after ----");
+	await onClose();
 });
 
 let port = 9200;
 
-test('GET /src', async (t) => {
-    const localURL = await t.context.start(t, `sable --port ${port++} --host localhost`);
-    const res = await fetch(new URL('/src', localURL.href).href);
-    t.is(res.status, 200);
-    t.is(res.headers.get('content-type'), 'text/html; charset=UTF-8');
-    const html = await res.text();
-    t.true(html.includes('<script'));
+test("GET /src", async (t) => {
+	const command = `sable --verbose --port ${port++}`;
+	const { localUrl, abc } = await start(command);
+	const res = await fetch(new URL("/src", localUrl), { signal: abc.signal });
+	assert.equal(res.status, 200);
+	assert.equal(res.headers.get("content-type"), "text/html; charset=UTF-8");
+	const html = await res.text();
+	assert.ok(html.includes('href="./index.html"'));
 });
 
-test('GET /src (documentRoot)', async (t) => {
-    const localURL = await t.context.start(t, `sable --port ${port++} --host localhost src`);
-    const res = await fetch(new URL('/', localURL.href).href);
-    t.is(res.status, 200);
+test("GET /src (documentRoot)", async (t) => {
+	const command = `sable --verbose --port ${port++} src`;
+	const { localUrl, abc } = await start(command);
+	const res = await fetch(new URL("/", localUrl), { signal: abc.signal });
+	assert.equal(res.status, 200);
+	const html = await res.text();
+	assert.ok(html.includes("test-src"));
 });
 
-test('GET /', async (t) => {
-    const localURL = await t.context.start(t, `sable --port ${port++} --host localhost`);
-    const res = await fetch(new URL('/', localURL.href).href);
-    t.is(res.status, 200);
+test("GET /", async (t) => {
+	const command = `sable --verbose --port ${port++}`;
+	const { localUrl, abc } = await start(command);
+	const res = await fetch(new URL("/", localUrl), { signal: abc.signal });
+	assert.equal(res.status, 200);
 });
 
-test('GET /index.mjs', async (t) => {
-    const config = {
-        host: 'localhost',
-        port: port++,
-        documentRoot: fileURLToPath(cwd),
-    };
-    const server = t.context.server = await startServer(config);
-    const addressInfo = server.address();
-    if (addressInfo && typeof addressInfo === 'object') {
-        const res = await fetch(new URL(`http://localhost:${addressInfo.port}/index.mjs`).href);
-        t.is(res.status, 200);
-    } else {
-        t.is(typeof addressInfo, 'object');
-    }
+test("GET /index.mjs", async (t) => {
+	const command = `sable --verbose --port ${port++}`;
+	const { localUrl, abc } = await start(command);
+	const res = await fetch(
+		new URL(`http://localhost:${localUrl.port}/index.mjs`),
+		{ signal: abc.signal },
+	);
+	assert.equal(res.status, 200);
 });
